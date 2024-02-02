54a3bb470f6fd7ea5ca05dd5496045da47d839f9
ddk9595 <027620@naver.com>
2024-02-02 17:48:47 +0900

    no message

---
+++ 999_selfStudy/src/oop/basic/Self_Account.java
@@ -0,0 +1,54 @@
+package oop.basic;
+
+public class Self_Account {
+
+	
+	public class Account {
+		
+		
+		// private 설정, 간접접근 구문(setter/getter)으로 접근 가능
+		private String name; 		 // 이름
+		private String accountNum;   // 계좌번호
+		private String password;     // 비밀번호
+		private long   balance;		 // 잔액
+		
+		// 이름 간접접근 구문
+		// setter
+		public void setName(String inputName) {
+			name = inputName;
+		}
+		
+		// getter
+		public String getName() {
+			return name;
+		}
+		
+		// 계좌명 setter
+		public void setAccountNum(String inputAccountNum) {
+			this.accountNum = inputAccountNum;
+		}
+		
+		// 계좌명 getter
+		public String getAcocuntNum() {
+			return accountNum;
+		}
+		
+		// 비밀번호
+		public void setPassword(String password) {
+			
+		}
+		
+		
+		
+		// 잔액
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
